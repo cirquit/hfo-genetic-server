58ad5d77217bf86538e5f51692bc86b0c9658c5a
cirquit <alex.isenko@googlemail.com>
2016-11-02 19:26:34 +0100

    updated plot script

--- hfo-server-hs/src/Evaluator.hs
+++ hfo-server-hs/src/Evaluator.hs
@@ -35,6 +35,7 @@ import Genetic.Selection
 
 resultsFile n = concat [ "/home/rewrite/Documents/Project-Repos/hfo-genetic-server/results/"
                      , "goal-fitness/"
+                     , "1v1/"
                      , "actiondist-25-trails/"
                      , "json-data/"
                      , "results" ++ show n ++ ".json"
@@ -67,7 +68,7 @@ testServerConf = defaultServer { untouchedTime    = 50
                                , defenseAgents    = 0
                                , offenseNpcs      = 0
                                , defenseNpcs      = 1
---                               , standartPace     = True
+                               , standartPace     = True
                                , giveBallToPlayer = 1 -- gives the ball to the first player...with the number 7
                                }
 
